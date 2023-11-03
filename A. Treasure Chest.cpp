18ff2ebcffb9cb28d98f7649cc563b6b8aa8fa40
Md Robiul Islam <148268570+roobiul@users.noreply.github.com>
2023-11-03 20:59:44 +0600

    Add files via upload

---
+++ A. Treasure Chest.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<algorithm>
+ 
+using namespace std;
+
+void solve()
+{
+    int x,y,k;
+    cin >> x>>y>>k;
+    if (x<y)
+    {
+        if (y-x<=k)
+        {
+            cout << y << endl;
+            return;
+        }
+        else 
+        {
+            cout << (2*y-(x+k))<< endl;
+            return;
+        }
+    }
+    cout << x<< endl;
+}
+ 
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t; 
+    cin>> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+ 
+ 
+return 0;
+}
